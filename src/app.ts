5bd08b32205d73050fc65c1a443949a268ce3b62
morshidulrahman <boydanger416@gmail.com>
2024-11-23 23:39:12 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { CarsRoute } from './app/modules/cars/cars.route';
 import { OrdersRoute } from './app/modules/orders/orders.route';
 
@@ -12,8 +12,16 @@ app.use(cors());
 app.use('/api', CarsRoute);
 app.use('/api', OrdersRoute);
 
-app.use('/', (req, res) => {
-  res.send('Welcome to the car API');
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the Car API',
+  });
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    error: 'Invalid Route',
+  });
 });
 
 export default app;
